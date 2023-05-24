898cc466ce59a4c5514c333e0fe9fe5b32454ea5
Christian Töppel <christian.toeppel@gmail.com>
2023-05-24 11:59:37 +0200

    implemented linter fixes
    v1.2

--- Backend/inventory.py
+++ Backend/inventory.py
@@ -1,29 +1,29 @@
+import os
 import psycopg2
 from dotenv import load_dotenv
 from flask import Flask, jsonify, request
-import os
 
 load_dotenv()
 
 app = Flask(__name__)
 
-host = os.environ.get('DB_HOST')
-port = os.environ.get('DB_PORT')
-database = os.environ.get('DB_DATABASE')
-user = os.environ.get('DB_USER')
-password = os.environ.get('DB_PASSWORD')
+host = os.environ.get("DB_HOST")
+port = os.environ.get("DB_PORT")
+database = os.environ.get("DB_DATABASE")
+user = os.environ.get("DB_USER")
+password = os.environ.get("DB_PASSWORD")
 
 conn = psycopg2.connect(
     host=host, port=port, database=database, user=user, password=password
 )
 
 
-@app.route('/')
+@app.route("/")
 def empty():
-    return 'leerer Pfad!'
+    return "leerer Pfad!"
 
 
-@app.route('/data', methods=['GET'])
+@app.route("/data", methods=["GET"])
 def get_data():
     cursor = conn.cursor()
     cursor.execute("SELECT id, name FROM test")
@@ -32,25 +32,25 @@ def get_data():
 
     results = []
     for row in data:
-        result = {'id': row[0], 'name': row[1]}
+        result = {"id": row[0], "name": row[1]}
         results.append(result)
     return jsonify(results)
 
 
-@app.route('/data', methods=['POST'])
+@app.route("/data", methods=["POST"])
 def add_data():
 
     data = request.get_json()
-    id = data['id']
-    name = data['name']
+    id = data["id"]
+    name = data["name"]
 
     cursor = conn.cursor()
     cursor.execute("INSERT INTO test (id, name) VALUES (%s, %s)", (id, name))
     conn.commit()
     cursor.close()
 
-    return 'Daten hinzugefügt.'
+    return "Daten hinzugefügt."
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     app.run()
