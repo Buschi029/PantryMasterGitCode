ae5e7501b657d8a4cb5b3e1fc7c2c38ebf6b8e25
Christian Töppel <christian.toeppel@gmail.com>
2023-05-25 15:08:49 +0200

    implemented functions for tbl_pantry table

--- Backend/inventory.py
+++ Backend/inventory.py
@@ -38,14 +38,34 @@ def get_data():
     return jsonify(results)
 
 
-@app.route("/data", methods=["POST"])
+@app.route("/inventory", methods=["GET"])
+def get_data():
+    cursor = conn.cursor()
+    cursor.execute("SELECT productCode, userID, productName, expirationDate, quantity, quantityUnit, appendDate, productCategory FROM tbl_pantry")
+    data = cursor.fetchall()
+    cursor.close()
+
+    results = []
+    for row in data:
+        result = {"id": row[0], "name": row[1]}
+        results.append(result)
+    return jsonify(results)
+
+
+@app.route("/inventory", methods=["POST"])
 def add_data():
     data = request.get_json()
-    id = data["id"]
-    name = data["name"]
+    productCode = data["productCode"]
+    userID = data["userID"]
+    productName = data["productName"]
+    expirationDate = data["expirationDate"]
+    quantity = data["quantity"]
+    quantityUnit = data["quantityUnit"]
+    appendDate = data["appendDate"]
+    productCategory = data["productCategory"]
 
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO test (id, name) VALUES (%s, %s)", (id, name))
+    cursor.execute("INSERT INTO tbl_pantry (productCode, userID, productName, expirationDate, quantity, quantityUnit, appendDate, productCategory) VALUES (%s, %s, %s, %s, %s, %s, %s, %s)", (productCode, userID, productName, expirationDate, quantity, quantityUnit, appendDate, productCategory))
     conn.commit()
     cursor.close()
 
