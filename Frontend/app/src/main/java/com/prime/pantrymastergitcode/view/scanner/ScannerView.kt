25d67e8edbd0994684bf6bfe773eb7d81582a39a
Moritz Dieterich <moritz_24@icloud.com>
2023-05-31 15:21:45 +0200

    wip

--- Frontend/app/src/main/java/com/prime/pantrymastergitcode/view/scanner/ScannerView.kt
+++ Frontend/app/src/main/java/com/prime/pantrymastergitcode/view/scanner/ScannerView.kt
@@ -3,6 +3,7 @@ package com.prime.pantrymastergitcode.view.scanner
 import android.util.Log
 import android.widget.Toast
 import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.layout.Arrangement.Absolute.Center
 import androidx.compose.material.Button
 import androidx.compose.material.Text
 import androidx.compose.material.TextField
@@ -13,6 +14,7 @@ import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.google.mlkit.vision.codescanner.GmsBarcodeScanning
+import com.prime.pantrymastergitcode.view.permissions.CameraPermission
 
 
 @Composable
@@ -24,35 +26,50 @@ fun ScannerView(){
     var productName by remember {mutableStateOf("")}
     var bestBefore by remember {mutableStateOf("")}
     var qantity by remember {mutableStateOf("")}
-    Column(
-        modifier = Modifier
-            .fillMaxWidth(),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.SpaceAround) {
-        Text(text = "Add New Product", fontSize = 30.sp)
-        Text(text = scannerTag)
-        Row() {
-            TextField(value = productName, onValueChange = {productName = it} )
-            TextField(value = bestBefore, onValueChange = {productName = it} )
-            TextField(value = productName, onValueChange = {productName = it} )
+    Box(modifier = Modifier.fillMaxWidth()){
+        Column(modifier = Modifier
+            .fillMaxSize(),
+        horizontalAlignment =Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) {
+            CameraPermission()
         }
 
-        Button(onClick = {  scanner.startScan()
-            .addOnSuccessListener { barcode ->
-                scannerTag = barcode.toString()
+        Column(
+            modifier = Modifier
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.SpaceAround
+        ) {
+            Text(text = "Add New Product", fontSize = 30.sp)
+            Text(text = scannerTag)
+            Row() {
+                TextField(value = productName, onValueChange = { productName = it })
+                TextField(value = bestBefore, onValueChange = { productName = it })
+                TextField(value = productName, onValueChange = { productName = it })
             }
-            .addOnCanceledListener {
-                // Task canceled
-                Log.i(TAG,"Scanning has been cancelled")
-            }
-            .addOnFailureListener { e ->
-                // Task failed with an exception
-                Log.e(TAG,e.toString())
-                Toast.makeText(context,"An Error occurred",Toast.LENGTH_SHORT).show()
-                scannerTag = e.toString()
-            } }) {
-            Text(text = "Scan")
 
+            Button(onClick = {
+                scanner.startScan()
+                    .addOnSuccessListener { barcode ->
+                        scannerTag = barcode.toString()
+                    }
+                    .addOnCanceledListener {
+                        // Task canceled
+                        Log.i(TAG, "Scanning has been cancelled")
+                    }
+                    .addOnFailureListener { e ->
+                        // Task failed with an exception
+                        Log.e(TAG, e.toString())
+                        Toast.makeText(context, "An Error occurred", Toast.LENGTH_SHORT).show()
+                        scannerTag = e.toString()
+                    }
+            }) {
+                Text(text = "Scan")
+
+            }
         }
+
     }
 }
+
