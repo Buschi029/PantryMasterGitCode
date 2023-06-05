6298a571c34931e5900b4bf4bc4baec3df640248
Buschi029 <basti.hecek@gmail.com>
2023-06-05 10:49:10 +0200

    ShoppingListFunctions add

--- Backend/shoppingList.py
+++ Backend/shoppingList.py
@@ -1,18 +1,16 @@
 import os
 
 import psycopg2
-from dotenv import load_dotenv
 from flask import Flask, jsonify, request
 
-load_dotenv()
 
 app = Flask(__name__)
 
-host = os.environ.get("DB_HOST")
-port = os.environ.get("DB_PORT")
-database = os.environ.get("DB_DATABASE")
-user = os.environ.get("DB_USER")
-password = os.environ.get("DB_PASSWORD")
+host = #"ep-old-rice-105179.eu-central-1.aws.neon.tech"
+port = #"5432"
+database = #"shoppingListDB"
+user = #"ADMIN"
+password = #"uihkP3cnT0Wo"
 
 conn = psycopg2.connect(
     host=host, port=port, database=database, user=user, password=password
@@ -24,64 +22,85 @@ def empty():
     return "leerer Pfad!"
 
 
-@app.route("/data", methods=["GET"])
-def get_data():
+@app.route("/shoppingList", methods=["GET"])
+def get_alldata():
     cursor = conn.cursor()
-    cursor.execute("SELECT id, name FROM test")
+    cursor.execute("SELECT * FROM tbl_shoppingList")
     data = cursor.fetchall()
     cursor.close()
 
     results = []
     for row in data:
-        result = {"id": row[0], "name": row[1]}
+        result = {"productName": row[0], "userID": row[1], "quantity": row[2], "quantityUnit": row[3]}
+        results.append(result)
+    return jsonify(results)
+
+@app.route("/shoppingList", methods=["UPDATE"])
+def get_data():
+    data = request.get_json()
+    userID = data["userID"]
+    cursor = conn.cursor()
+    cursor.execute("SELECT * FROM tbl_shoppingList WHERE userID=%s", (userID))
+    data = cursor.fetchall()
+    cursor.close()
+
+    results = []
+    for row in data:
+        result = {"productName": row[0], "userID": row[1], "quantity": row[2], "quantityUnit": row[3]}
         results.append(result)
     return jsonify(results)
 
 
-# @app.route("/shoppingList", methods=["GET"])
-# def get_data():
-#     cursor = conn.cursor()
-#     cursor.execute("SELECT productCode, userID, productName, quantity, quantityUnit, productCategory FROM tbl_pantry")
-#     data = cursor.fetchall()
-#     cursor.close()
-
-#     results = []
-#     for row in data:
-#         result = {"id": row[0], "name": row[1]}
-#         results.append(result)
-#     return jsonify(results)
-
-
-# @app.route("/shoppingList", methods=["POST"])
-# def add_data():
-#     data = request.get_json()
-#     productCode = data["productCode"]
-#     userID = data["userID"]
-#     productName = data["productName"]
-#     quantity = data["quantity"]
-#     quantityUnit = data["quantityUnit"]
-#     productCategory = data["productCategory"]
-
-#     cursor = conn.cursor()
-#     cursor.execute("INSERT INTO tbl_pantry (productCode, userID, productName, quantity, quantityUnit, productCategory) VALUES (%s, %s, %s, %s, %s, %s)", (productCode, userID, productName, quantity, quantityUnit, productCategory))
-#     conn.commit()
-#     cursor.close()
-
-#     return "Daten hinzugef�gt."
-
-@app.route("/test", methods=["POST"])
+@app.route("/shoppingList", methods=["POST"])
 def add_data():
     data = request.get_json()
-    name = data["name"]
+    userID = data["userID"]
+    productName = data["productName"]
     quantity = data["quantity"]
+    quantityUnit = data["quantityUnit"]
 
+    output = checkForDuplicates(productName, userID, quantity, quantityUnit)
+
+    return output
+
+def checkForDuplicates(productName, userID, quantity, quantityUnit):
     cursor = conn.cursor()
-    cursor.execute("INSERT INTO test (name, quantity) VALUES (%s, %s)", (name, quantity))
+    cursor.execute("SELECT productName FROM tbl_shoppingList WHERE productName=%s AND userID=%s", (productName, userID))
+
+    if cursor.rowcount == 0:
+        return addEntry(productName, userID, quantity, quantityUnit)
+    else:
+        return updateEntry(productName, userID, quantity)
+
+def updateEntry(productName, userID, quantity):
+    cursor = conn.cursor()
+    cursor.execute("Update tbl_shoppingList SET quantity=quantity+%s WHERE productName=%s AND userID=%s", (quantity, productName, userID))
+    cursor.close()
+
+    return "Eintrag erhöht"
+
+def addEntry(productName, userID, quantity, quantityUnit):
+    cursor = conn.cursor()
+    cursor.execute("INSERT INTO tbl_shoppingList (userID, productName, quantity, quantityUnit) VALUES (%s, %s, %s, %s)", (userID, productName, quantity, quantityUnit))
     conn.commit()
     cursor.close()
 
-    return "Daten hinzugef�gt."
+    return "Eintrag angelegt"
 
 
-if __name__ == "__main__":
-    app.run()
+@app.route("/shoppingList", methods=["DELETE"])
+def delete_data():
+    data = request.get_json()
+    userID = data["userID"]
+    productName = data["productName"]
+
+    cursor = conn.cursor()
+    cursor.execute("DELETE FROM tbl_shoppingList WHERE productName=%s AND userID=%s", (productName, userID))
+    conn.commit()
+    cursor.close()
+
+    return "Entfernt"
+
+
+
+app.run(host='0.0.0.0', port=81)
