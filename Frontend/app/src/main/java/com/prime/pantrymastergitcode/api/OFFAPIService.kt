931207d07715559a818594bef35347f2b74ff962
Moritz Dieterich <moritz_24@icloud.com>
2023-06-07 15:38:10 +0200

    Linter Changes 1.3

--- Frontend/app/src/main/java/com/prime/pantrymastergitcode/api/OFFAPIService.kt
+++ Frontend/app/src/main/java/com/prime/pantrymastergitcode/api/OFFAPIService.kt
@@ -1,6 +1,5 @@
 package com.prime.pantrymastergitcode.api
 
-import com.prime.pantrymastergitcode.api.dto.ProductDTO
 import io.ktor.client.engine.cio.CIO
 import io.ktor.client.HttpClient
 import io.ktor.client.plugins.contentnegotiation.ContentNegotiation
@@ -8,6 +7,7 @@ import io.ktor.client.plugins.logging.Logging
 import io.ktor.client.plugins.logging.LogLevel
 import io.ktor.serialization.kotlinx.json.json
 import kotlinx.serialization.json.Json
+import com.prime.pantrymastergitcode.api.dto.ProductDTO
 
 interface OFFAPIService {
 
