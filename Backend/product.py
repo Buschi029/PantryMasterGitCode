040146827f8935aa81e03c685a9e7a05b7ac9735
Chris <christian.toeppel@gmail.com>
2023-06-15 21:31:18 +0200

    unit tests v6

--- Backend/product.py
+++ Backend/product.py
@@ -3,9 +3,11 @@ import json
 import psycopg2
 import requests
 from flask import Flask, jsonify, request
-#from __main__ import app
+from __main__ import app
 
-app = Flask(__name__)
+#python -m unittest discover -s 'Backend/' -p 'test_product.py'
+
+#app = Flask(__name__)
 
 host = "ep-old-rice-105179.eu-central-1.aws.neon.tech"
 port = "5432"
