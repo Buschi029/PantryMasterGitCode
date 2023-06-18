6b02f802d182534771625cc1bc0c10fb645c0c48
Chris <christian.toeppel@gmail.com>
2023-06-18 16:05:25 +0200

    updated unit test

--- Backend/test_product.py
+++ Backend/test_product.py
@@ -7,29 +7,10 @@ class FlaskTestCase(unittest.TestCase):
     def setUp(self):
         self.app = app.test_client()
 
-    def test_add_data_new_product(self):
-        
-        # Testfall, wenn Produkt nicht in der Datenbank existiert
-        product_data = {
-            "barcode": "42143574"
-        }
-
-        response = self.app.post(
-            "/product", 
-            data=json.dumps(product_data), 
-            content_type='application/json'
-        )
-
-        self.assertEqual(response.status_code, 200)
-        data = json.loads(response.data)
-        self.assertEqual(data["productcode"], 42143574)
-
-
     def test_add_data_existing_product(self):
 
-        # Testfall, wenn Produkt bereits in der Datenbank existiert
         product_data = {
-            "barcode": "5000159452540"
+            "barcode": "4103840026417"
         }
 
         response = self.app.post(
@@ -40,7 +21,7 @@ class FlaskTestCase(unittest.TestCase):
 
         self.assertEqual(response.status_code, 200)
         data = json.loads(response.data)
-        self.assertEqual(data["productcode"], 5000159452540)
+        self.assertEqual(data["productcode"], 4103840026417)
 
 
 if __name__ == "__main__":
