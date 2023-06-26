4b314d7306611a67b1ee5d74fcebe7b5987d6a49
Chris <christian.toeppel@gmail.com>
2023-06-26 15:06:12 +0200

    updated unit tests v1.3

--- Backend/inventoryBackend/test_product.py
+++ Backend/inventoryBackend/test_product.py
@@ -1,19 +1,22 @@
 import unittest
 import json
+
 from app import app
 
+
 class TestFlaskRoutes(unittest.TestCase):
+
     def setUp(self):
         self.app = app.test_client()
-        self.app.testing = True 
+        self.app.testing = True
 
     def test_get_oneProduct(self):
-        mock_request_headers = {'Content-Type': 'application/json'}
-        mock_request_data = json.dumps({
-            "barcode": "4103840026417"  
-        })
+        mock_request_headers = {"Content-Type": "application/json"}
+        mock_request_data = json.dumps({"barcode": "4103840026417"})
 
-        response = self.app.post('/product', headers=mock_request_headers, data=mock_request_data)
+        response = self.app.post(
+            "/product", headers=mock_request_headers, data=mock_request_data
+        )
         self.assertEqual(response.status_code, 200)
 
         data = response.get_json()
@@ -26,7 +29,11 @@ class TestFlaskRoutes(unittest.TestCase):
         self.assertEqual(data["protein"], 12)
         self.assertEqual(data["sugar"], 3)
         self.assertEqual(data["name"], "High Protein Quark Creme - Pfirsich Maracuja")
-        self.assertEqual(data["pictureLink"], "https://images.openfoodfacts.org/images/products/410/384/002/6417/front_de.3.400.jpg")
+        self.assertEqual(
+            data["pictureLink"],
+            "https://images.openfoodfacts.org/images/products/410/384/002/6417/front_de.3.400.jpg",
+        )
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     unittest.main()
