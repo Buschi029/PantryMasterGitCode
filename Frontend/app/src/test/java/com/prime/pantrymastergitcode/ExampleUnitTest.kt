7e2728e10252f17a60577d64473377b7fa782c62
Chris <christian.toeppel@gmail.com>
2023-06-28 17:06:02 +0200

    updated unit test

--- Frontend/app/src/test/java/com/prime/pantrymastergitcode/ExampleUnitTest.kt
+++ Frontend/app/src/test/java/com/prime/pantrymastergitcode/ExampleUnitTest.kt
@@ -1,30 +1,21 @@
 package com.prime.pantrymastergitcode
 
 import com.prime.pantrymastergitcode.api.OFFAPIService
-import com.prime.pantrymastergitcode.view.scanner.ScannerViewModel
 import kotlinx.coroutines.runBlocking
-import kotlinx.coroutines.test.runTest
+import org.junit.Assert.*
 import org.junit.Test
 
-import org.junit.Assert.*
-
-/**
- * Example local unit test, which will execute on the development machine (host).
- *
- * See [testing documentation](http://d.android.com/tools/testing).
- */
 class ExampleUnitTest {
     private val service = OFFAPIService.create()
 
     @Test
-    fun getProductDetails() = runTest {
-        val product = service.postProductDetails(4000140703881)
-        assertNotNull("Product should not be null", product)
-        if (product != null) {
-            assertEquals("Produkt wurde aus Backend geladen", "Orangina original", product.name)
+    fun getProductDetails() {
+        runBlocking {
+            val product = service.postProductDetails(4000140703881)
+            assertNotNull("Product should not be null", product)
+            if (product != null) {
+                assertEquals("Produkt wurde aus Backend geladen", "Orangina original", product.name)
+            }
         }
     }
-}
-
-
 }
