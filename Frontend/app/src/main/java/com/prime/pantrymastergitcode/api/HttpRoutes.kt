b97f749764af32e9447cdd2d21f1a22da800436d
Moritz Dieterich <moritz_24@icloud.com>
2023-06-28 12:38:28 +0200

    changed URL

--- Frontend/app/src/main/java/com/prime/pantrymastergitcode/api/HttpRoutes.kt
+++ Frontend/app/src/main/java/com/prime/pantrymastergitcode/api/HttpRoutes.kt
@@ -1,7 +1,7 @@
 package com.prime.pantrymastergitcode.api
 
 object HttpRoutes {
-    private const val BASE_URL = "http://192.168.0.177:"
+    private const val BASE_URL = "http://localhost:"
     private const val BASE_URL_PRODUCT = "${BASE_URL}8080/"
     const val product = "${BASE_URL_PRODUCT}product"
     const val inventory = "${BASE_URL_PRODUCT}inventory"
