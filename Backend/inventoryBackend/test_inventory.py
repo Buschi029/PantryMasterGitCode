03e031634fa7ec8fd968c99258c58031db5d0689
Christian Töppel <christian.toeppel@gmail.com>
2023-07-02 11:41:27 +0200

    updated unit tests

--- Backend/inventoryBackend/test_inventory.py
+++ Backend/inventoryBackend/test_inventory.py
@@ -21,7 +21,7 @@ class TestFlaskRoutes(unittest.TestCase):
             "expirationDate": "2024-07-10",
             "quantity": "1",
             "quantityUnit": "kg",
-            "appendDate": "2023-06-26"
+            "appendDate": "2023-06-26T00:00:00.0000"
         })
         
         response = self.app.post('/inventory', headers=mock_request_headers, data=mock_request_data)
@@ -31,8 +31,7 @@ class TestFlaskRoutes(unittest.TestCase):
     def test_delete_invItem(self):
         mock_request_headers = {'Content-Type': 'application/json'}
         mock_request_data = json.dumps({
-            "userID": "1",
-            "barcode": "12345"
+            "appendTime": "2023-06-26T00:00:00.0000",
         })
         
         response = self.app.delete('/inventory', headers=mock_request_headers, data=mock_request_data)
